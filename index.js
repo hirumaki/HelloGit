f2919a0f509deab795c215e78cb8ad0540b4d47a
hirumaki <index3583@gmail.com>
2021-05-29 15:08:17 +0900

    add coins

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
+let money = 0;
+
 function buttonClick(msg){
     const result = document.getElementById(msg);
     result.style.display = 'block';
 }
 
+function addMoney(added){
+    money += added
+    document.getElementById('moneyarea').textContent = money;
+}
