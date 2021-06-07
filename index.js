3b8c3d4b16f2e86d7dda6bbc48a257b1e0765280
hirumaki <index3583@gmail.com>
2021-06-07 12:09:01 +0900

    v1

--- index.js
+++ index.js
@@ -1,11 +1,35 @@
 let money = 0;
 
-function buttonClick(msg){
-    const result = document.getElementById(msg);
-    result.style.display = 'block';
+const purchaseProduct = (productName) =>{
+    console.log(productName);
+    const product = document.getElementById(productName);
+    console.log(product);
+    const price = product.getElementsByClassName('price')[0].attributes[1].value;
+    money -= price 
+    document.getElementById('moneyarea').textContent = money;
+    document.getElementById('purchased').innerHTML += `<p>${productName}を買いました。</p>`;
+    resetButtons();
 }
 
-function addMoney(added){
+
+const resetButtons = () => {
+    const products = document.getElementsByClassName('product');
+    for(let i =0;i < products.length ; i++){
+        const product = products[i];
+        const price = product.getElementsByClassName('price')[0].attributes[1].value;
+        if(money >= price){
+            product.getElementsByClassName('product-button')[0].removeAttribute('disabled');
+        }
+        else{
+            product.getElementsByClassName('product-button')[0].setAttribute('disabled',true);
+        }
+    }
+}
+
+const addMoney = (added)=>{
     money += added
     document.getElementById('moneyarea').textContent = money;
+    resetButtons();
 }
+
+resetButtons();
